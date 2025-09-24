96c705ab9db97c4a98729d441b383c4e4490a2d9
dyash <dyash@yash007>
2025-09-24 21:08:16 +0530

    THIRD COMMIT

--- DemoTnsif/src/day_1/Simple.java
+++ DemoTnsif/src/day_1/Simple.java
@@ -1,9 +1,31 @@
 package day_1;
 
+import java.util.Scanner;
+
 public class Simple {
  public static void main(String[] args) {
-	System.out.println("happy world");
-	System.out.println("lets go");
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the marks");
+	int marks = sc.nextInt();
+	if(marks<35) {
+		System.out.println("Fail");
+	}
+	else if(marks>35&&marks<=50) {
+		System.out.println("Grade D");
+	}
+	else if(marks>50&&marks<=60) {
+		System.out.println("Grade C");
+	}
+	else if(marks>60&&marks<=70) {
+		System.out.println("Grade B");
+	}
+	else if(marks>70&&marks<=80) {
+		System.out.println("Grade A");
+	}
+	else {
+		System.out.println("Destinction");
+	}
+	sc.close();
 }
 
 }
